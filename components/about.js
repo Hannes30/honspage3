32626ad8ee59a971140e1bc2fd4dc042944401ba
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2023-03-01 10:30:14 +0100

    about me improvements and animation

--- components/about.js
+++ components/about.js
@@ -6,24 +6,13 @@ const About = () => {
       <div className={styles.left}>
         <div className={styles.heading}>About me</div>
         <div className={styles.text}>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-          amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-          At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-          amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit
-          esse molestie consequat, vel illum dolore eu feugiat nulla facilisis
-          at vero eros et accumsan et iusto odio dignissim qui blandit praesent
-          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-          Lorem ipsum dolor sit amet,
+          Hello! I'm a 17-year-old student currently studying at IT-HTL Ybbs. I
+          have a passion for turning my ideas into reality through coding. My
+          specialties include using JavaScript for web development, Adobe
+          products for design, and Flutter for mobile development. During my
+          free time, I also enjoy developing Unity games. I'm constantly
+          exploring new technologies and expanding my skill set, and I'm excited
+          to see where my passion for coding takes me in the future.
         </div>
       </div>
       <div className={styles.right}>
--- components/aboutMeImage.js
+++ components/aboutMeImage.js
@@ -1,17 +1,70 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "@/styles/AboutMeImage.module.css";
 import Image from "next/image";
 
 const AboutMeImage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setTimeout(() => {
+            setIsVisible(true);
+          }, 500);
+        }
+      },
+      {
+        rootMargin: "0px",
+        threshold: 0.5, // Change this value to adjust when the intersection is detected
+      }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <div className={styles.main}>
         <Image
           src="/images/about-image-Hannes.png"
-          width="430"
-          height="400"
+          width="330"
+          height="300"
           className={styles.image}
-        ></Image>
-        <div className={styles.imageBackground}></div>
+        />
+        <div className={styles.imageBackground} />
+        <div className={styles.icons}>
+          <Image
+            src="/images/Laptop.png"
+            width="80"
+            height="80"
+            className={`${styles.icon} ${isVisible ? styles.icon0 : ""}`}
+            ref={ref}
+          />
+          <Image
+            src="/images/camera.png"
+            width="80"
+            height="80"
+            className={`${styles.icon} ${isVisible ? styles.icon1 : ""}`}
+            ref={ref}
+          />
+          <Image
+            src="/images/smartphone.png"
+            width="80"
+            height="80"
+            className={`${styles.icon} ${isVisible ? styles.icon2 : ""}`}
+            ref={ref}
+          />
+        </div>
       </div>
     </>
   );
