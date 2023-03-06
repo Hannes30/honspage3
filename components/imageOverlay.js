fd7317ed6a66f267177ff36632f49776d9a4c279
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2023-03-06 13:30:40 +0100

    bug fixes

--- components/imageOverlay.js
+++ components/imageOverlay.js
@@ -5,9 +5,7 @@ import Link from "next/link";
 const ImageOverlay = () => {
   return (
     <>
-      <Link href={"./"}>
-        <div className={styles.image}></div>
-      </Link>
+      <div className={styles.image}></div>
     </>
   );
 };
