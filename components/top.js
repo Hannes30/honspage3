f8b9eabb87274775e9ec31db9f01e64369445293
Hannes0_0 <hannes.scheibelauer@sz-ybbs.ac.at>
2023-03-30 18:20:58 +0200

    scroll indication arrow

--- components/top.js
+++ components/top.js
@@ -2,7 +2,21 @@ import styles from "@/styles/top.module.css";
 import HeadingText from "@/components/headingText";
 import Flyins from "./flyins";
 import ImageOverlay from "./imageOverlay";
+import Image from "next/image";
+import { use, useEffect, useState } from "react";
 const Top = () => {
+  const [imageVisible, setImageVisible] = useState(true);
+
+  useEffect(
+    () =>
+      window.addEventListener("scroll", () => {
+        if (window.pageYOffset > 50) {
+          imageVisible ? setImageVisible(false) : "";
+        }
+      }),
+    []
+  );
+
   return (
     <div className={styles.main}>
       <div className={styles.left}>
@@ -12,6 +26,13 @@ const Top = () => {
         <Flyins />
         <ImageOverlay />
       </div>
+      <Image
+        className={styles.arrow + " " + (!imageVisible ? styles.hidden : "")}
+        src="/images/arrow.png"
+        width="100"
+        height="100"
+        alt="arrrow"
+      ></Image>
     </div>
   );
 };
