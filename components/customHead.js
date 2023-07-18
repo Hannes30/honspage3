c395ba89893a26c699a65fd12802b4adf555caaf
Hannes30 <hannes.scheibelauer@sz-ybbs.ac.at>
2023-07-18 15:47:44 +0200

    googel analytics

--- components/customHead.js
+++ components/customHead.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import dotenv from "dotenv";
+dotenv.config();
 
 const CustomHead = () => {
   return (
@@ -26,6 +28,18 @@ const CustomHead = () => {
         property="og:image"
         content="https://hannes-scheibelauer.com/images/logo.png"
       ></meta>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+      ></script>
+      <script>
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.GA_TRACKING_ID}');
+    `}
+      </script>
     </Head>
   );
 };
