f412aa9bad44148ef32f1e1bca7158cbba5ed4c4
Hannes0_0 <hannes.scheibelauer@sz-ybbs.ac.at>
2023-08-21 19:40:36 +0200

    fix: webp and resolution optimized

--- components/about.js
+++ components/about.js
@@ -6,7 +6,15 @@ const About = () => {
       <div className={styles.left}>
         <div className={styles.heading}>About me</div>
         <div className={styles.text}>
-        Hallo! Ich bin ein 17-jähriger Schüler, der derzeit die IT-HTL Ybbs besucht. Ich habe eine Leidenschaft dafür, meine Ideen durch das Programmieren in die Realität umzusetzen. Meine Spezialgebiete umfassen die Verwendung von JavaScript für die Webentwicklung, Adobe-Produkte für das Design und Flutter für die Entwicklung von mobilen Anwendungen. In meiner Freizeit entwickle ich auch gerne Unity-Spiele. Ich erkunde ständig neue Technologien und erweitere mein Fähigkeiten, und ich bin gespannt zu sehen, wohin mich meine Leidenschaft für das Programmieren in der Zukunft führen wird.
+          Hallo! Ich bin ein 17-jähriger Schüler, der derzeit die IT-HTL Ybbs
+          besucht. Ich habe eine Leidenschaft dafür, meine Ideen durch das
+          Programmieren in die Realität umzusetzen. Meine Spezialgebiete
+          umfassen die Verwendung von JavaScript für die Webentwicklung,
+          Adobe-Produkte für das Design und Flutter für die Entwicklung von
+          mobilen Anwendungen. In meiner Freizeit entwickle ich auch gerne
+          Unity-Spiele. Ich erkunde ständig neue Technologien und erweitere mein
+          Fähigkeiten, und ich bin gespannt zu sehen, wohin mich meine
+          Leidenschaft für das Programmieren in der Zukunft führen wird.
         </div>
       </div>
       <div className={styles.right}>
