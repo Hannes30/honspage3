b69277bcc7cca2a5e1b98cea76ca0f243123b11b
Hannes Scheibelauer <hannes.scheibelauer@sz-ybbs.ac.at>
2023-08-22 09:50:43 +0200

    fix: removed unused import statements

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Head from "next/head";
+
 import CustomHead from "@/components/customHead";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
 import Top from "@/components/top";
 import About from "@/components/about";
 import Projects from "@/components/projects";
 import Contact from "@/components/contact";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
